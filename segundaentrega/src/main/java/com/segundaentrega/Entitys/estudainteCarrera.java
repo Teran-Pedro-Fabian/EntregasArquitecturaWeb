5467ac9d1e89e59ce08c4b227d3c1e9f158f8b14
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2026-09-18 11:22:04 -0300

    inicio de las entitys, agrego DB, configuaracion del pom

---
+++ segundaentrega/src/main/java/com/segundaentrega/Entitys/estudainteCarrera.java
@@ -0,0 +1,15 @@
+package com.segundaentrega.Entitys;
+
+
+
+public class estudainteCarrera {
+    private int id;
+    private int id_estudiante;
+    private int id_carrera;
+    private int inscripcion;
+    private int graduacion;
+    private int antiguedad; 
+
+
+
+}
