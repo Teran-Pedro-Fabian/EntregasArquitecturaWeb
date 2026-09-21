465590bec7a9102ecec421159878ce8ad24de9fc
Pedro Fabian Teran <teranpedrofabian@gmail.com>
2026-09-21 18:32:32 -0300

    agrego metodos a estudiante, carrera y estudianteCarreraRepository

--- segundaentrega/src/main/java/com/segundaentrega/Repository/CarreraRepository.java
+++ segundaentrega/src/main/java/com/segundaentrega/Repository/CarreraRepository.java
@@ -1,3 +1,26 @@
-public class CarreraRepository {
+package com.segundaentrega.Repository;
 
+import com.segundaentrega.Entitys.CarreraEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface CarreraRepository extends JpaRepository<CarreraEntity, Integer> {
+
+
+    @Query("""
+            UPDATE CarreraEntity c
+            SET c.nombre = :#{#carrera.nombre},
+                c.cantidadMaterias = :#{#carrera.cantidadMaterias},
+                c.cantidadAnios = :#{#carrera.cantidadAnios}
+            WHERE c.id = :#{#carrera.id}
+            """)
+    public int UpdateCarrera(@Param("carrera") CarreraEntity carrera);
+
+
+    @Query ("""
+            INSERT INTO CarreraEntity (nombre, cantidadMaterias, cantidadAnios)
+            VALUES (:#{#carrera.nombre}, :#{#carrera.cantidadMaterias}, :#{#carrera.cantidadAnios})
+            """)
+    public int InsertCarrera(@Param("carrera") CarreraEntity carrera);
 }
--- segundaentrega/src/main/java/com/segundaentrega/Repository/EstudianteCarreraRepository.java
+++ segundaentrega/src/main/java/com/segundaentrega/Repository/EstudianteCarreraRepository.java
@@ -1,3 +1,71 @@
-public class EstudianteCarreraRepository {
+package com.segundaentrega.Repository;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.segundaentrega.Entitys.EstudainteCarrera;
+
+public interface EstudianteCarreraRepository extends JpaRepository<EstudainteCarrera, Integer> {
+
+    @Query("""
+            SELECT ec
+            FROM EstudianteCarrera ec
+            WHERE ec.id_carrera =:idCarrera
+            """)
+    public EstudainteCarrera FindByIdCarrera(@Param("idCarrera") int idCarrera);
+
+
+    @Query ("""
+            SELECT ec.id_carrera
+            FROM EstudianteCarrera ec
+            """)
+    public List<EstudainteCarrera> FindAll();
+
+    @Query ("""
+            SELECT ec
+            FROM EstudianteCarrera ec
+            WHERE ec.id_estudiante =:idEstudiante
+            """)
+    public EstudainteCarrera findByEstudiante(@Param("idEstudiante") int idEstudiante);
+
+
+    @Query ("""
+            INSERT INTO EstidainteCarrera(id, id_estudiante, id_carrera, inscripcion, graduacion, antiguedad)
+            VALUES (:#{#estCarr.id}, :#{#estCarr.id_estudiante}, :#{#estCarr.id_carrera}, :#{#inscripcion}, 
+            :#{#estCarr.graduacion}, :#{#estCarr.antiguedad})
+            """)
+    public void Insert(@Param("estCarr") EstudainteCarrera estCarr);
+
+
+    @Query("""
+            UPDATE EstudainteCarrera ec 
+            SET ec.id_estudiante = :#{#estCarr.id_estudiante}, 
+                ec.id_carrera = :#{#estCarr.id_carrera},
+                ec.inscripcion = :#{#estCarr.inscripcion},
+                ec.graduacion = :#{#estCarr.graduacion},
+                ec.antiguedad = :#{#estCarr.antiguedad}
+            WHERE ec.id = :#{#estCarr.id}
+            """)
+    public void Update(@Param("estCarr") EstudainteCarrera estCarr);
+
+    @Query("""
+            DELETE 
+            FROM EstudainteCarrera ec
+            WHERE ec.id =: id 
+            """)
+    public void delete(@Param("id") int id);
+
+
+    @Query("""
+            SELECT ec , 
+            FROM EstudainteCarrera ec
+            ORDER BY ec.id_estudiante
+            """)
+    public List<EstudainteCarrera> FindAllOrderByEstudiantes();
+
+
+    public List<Estudiante> FindAllEstudianteFilter(carrera C)
 }
