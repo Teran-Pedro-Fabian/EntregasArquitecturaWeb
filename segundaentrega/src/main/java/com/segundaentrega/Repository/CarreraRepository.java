5ffef5ec85d37697f17ad9df6de358c5e89ba065
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-09-23 17:43:02 -0300

    Correcciones CarreraRepository
    Coreccion UpdateCarrera para utilizar los atributos reales de CarreraEntity
    Coreccion InsertCarrera para utilizar id_carrera, carrera y duracion
    Agregado @Modifying para indicar las consultas que modifican la BD
    Agregado @Transactional para ejecutar las modificaciones dentro de una transaccion
    nativeQuery en InsertCarrera

--- segundaentrega/src/main/java/com/segundaentrega/Repository/CarreraRepository.java
+++ segundaentrega/src/main/java/com/segundaentrega/Repository/CarreraRepository.java
@@ -1,26 +1,31 @@
 package com.segundaentrega.Repository;
 
 import com.segundaentrega.Entitys.CarreraEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface CarreraRepository extends JpaRepository<CarreraEntity, Integer> {
 
-
+    @Modifying
+    @Transactional
     @Query("""
             UPDATE CarreraEntity c
-            SET c.nombre = :#{#carrera.nombre},
-                c.cantidadMaterias = :#{#carrera.cantidadMaterias},
-                c.cantidadAnios = :#{#carrera.cantidadAnios}
+            SET c.carrera = :#{#carrera.carrera},
+                c.duracion = :#{#carrera.duracion}
             WHERE c.id = :#{#carrera.id}
             """)
     public int UpdateCarrera(@Param("carrera") CarreraEntity carrera);
 
 
-    @Query ("""
-            INSERT INTO CarreraEntity (nombre, cantidadMaterias, cantidadAnios)
-            VALUES (:#{#carrera.nombre}, :#{#carrera.cantidadMaterias}, :#{#carrera.cantidadAnios})
-            """)
+    @Modifying
+    @Transactional
+    @Query(value = """
+            INSERT INTO carrera (id_carrera, carrera, duracion)
+            VALUES (:#{#carrera.id}, :#{#carrera.carrera}, :#{#carrera.duracion})
+            """, nativeQuery = true)
     public int InsertCarrera(@Param("carrera") CarreraEntity carrera);
 }
